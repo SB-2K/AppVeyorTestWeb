2f16ba65924d12398d48ea2e48a821ebacccc27a
Leon <leon@worketc.com>
2017-11-21 19:01:36 +0800

    test project

---
+++ AppVeyorTestWeb/EntityService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AppVeyorTestWeb
+{
+    public class EntityService
+    {
+        public Entity GetEntity(int EntityID)
+        {
+            return new Entity() {EntityID=EntityID, Name="Entity Name("+EntityID+")" };
+        }
+    }
+}
---
+++ Test/UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AppVeyorTestWeb;
+
+namespace Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestGetEntity()
+        {
+            EntityService service = new EntityService();
+            Entity entity = service.GetEntity(1);
+            Assert.Equals(entity.EntityID, 1);
+        }
+    }
+}
